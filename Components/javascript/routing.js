c69d4657737cfc963c93350ff0aa7d8822062207
johnnymanolly <manollyjohnny@gmail.com>
2017-11-17 13:21:11 +0200

    updates

---
+++ Components/javascript/routing.js
@@ -0,0 +1,26 @@
+var headerItems = {
+  //"logo": "",
+  "items": [],
+      "appname": "UI components"
+};
+
+var routingItems = {
+  "params": [
+    {"route": "barChart", "template": "/Components/html/examples/barChart.html"},
+    {"route": "areaChart", "template": "/Components/html/examples/areaChart.html"},
+    {"route": "lineChart", "template": "/Components/html/examples/lineChart.html"},
+    {"route": "donutChart", "template": "/Components/html/examples/donutChart.html"},
+    {"route": "accelerometer", "template": "/Components/html/examples/accelerometer.html"},
+    {"route": "button", "template": "/Components/html/examples/button.html"},
+    {"route": "gauge", "template": "/Components/html/examples/gauge.html"},
+    {"route": "grid", "template": "/Components/html/examples/grid.html"},
+    {"route": "map", "template": "/Components/html/examples/map.html"},
+    {"route": "odometer", "template": "/Components/html/examples/odometer.html"},
+    {"route": "progressBar", "template": "/Components/html/examples/progressBar.html"},
+    {"route": "slider", "template": "/Components/html/examples/slider.html"},
+    {"route": "speedometer", "template": "/Components/html/examples/speedometer.html"},
+    {"route": "thermometer", "template": "/Components/html/examples/thermometer.html"},
+    {"route": "toggleSwitch", "template": "/Components/html/examples/toggleSwitch.html"}
+  ],
+  "otherwiseOption" : {"template": "/"}
+};
